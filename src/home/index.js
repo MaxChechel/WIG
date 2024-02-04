43e41cddfce0ed0e1f9df23f9743434462613c3e
Max Chechel <maksymchechel@yahoo.com>
2024-02-04 16:28:13 +0100

    nav links hover

--- src/home/index.js
+++ src/home/index.js
@@ -8,22 +8,6 @@ let mm = gsap.matchMedia();
 document.addEventListener("DOMContentLoaded", () => {
   //Hovers
   mm.add("(hover:hover)", () => {
-    //Inline links hover
-    const inlineLinks = document.querySelectorAll(".navbar_link");
-    inlineLinks.forEach((link) => {
-      const textItem = link.querySelector(".inline-link_text");
-      const text = textItem.textContent;
-
-      link.addEventListener("mouseover", () => {
-        setTimeout(() => {
-          scramble(textItem, "Send message");
-        }, 150);
-      });
-      link.addEventListener("mouseleave", () => {
-        setTimeout(() => {
-          scramble(textItem, text);
-        }, 150);
-      });
-    });
+    navLinksHadler();
   });
 });
