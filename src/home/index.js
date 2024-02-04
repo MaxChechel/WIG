cc4ef076768ebf139fd9185215941bb3d28724dc
Max Chechel <maksymchechel@yahoo.com>
2024-02-04 16:29:19 +0100

    Update index.js

--- src/home/index.js
+++ src/home/index.js
@@ -2,6 +2,8 @@ import { gsap } from "gsap";
 import { Flip } from "gsap/all";
 import ScrambleTextPlugin from "gsap/dist/ScrambleTextPlugin";
 
+import navLinksHadler from "../components/navLinksHandler";
+
 gsap.registerPlugin(Flip, ScrambleTextPlugin);
 
 let mm = gsap.matchMedia();
