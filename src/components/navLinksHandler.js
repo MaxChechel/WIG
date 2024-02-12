6e0977249917c352c00cdecf307a43a74a1c7d99
Max Chechel <maksymchechel@yahoo.com>
2024-02-12 09:03:56 +0100

    Update navLinksHandler.js

--- src/components/navLinksHandler.js
+++ src/components/navLinksHandler.js
@@ -129,7 +129,7 @@ export default function navLinksHadler() {
 
     function linkClickHandler(link) {
       //e.preventDefault();
-      console.log(e.target);
+      //console.log(e.target);
       navLinks.forEach((link) => {
         const name = link.getAttribute("data-nav-link");
 
