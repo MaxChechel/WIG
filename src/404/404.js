6631d0723fe844887f3985dc7a5d71d8a7f5f0f9
Max Chechel <maksymchechel@yahoo.com>
2024-03-06 07:51:02 +0100

    Create 404.js

---
+++ src/404/404.js
@@ -0,0 +1,26 @@
+import tagsCount from "../utility/tagsCount";
+import navLinksHadler from "../components/navLinksHandler";
+
+fetch(`/category/${slug}`)
+  .then((response) => {
+    // Check if the request was successful
+    if (response.ok) {
+      return response.text(); // Return the response text (HTML content)
+    }
+    throw new Error("Network response was not ok.");
+  })
+  .then((html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    // Example: Count elements with a specific class
+    const nav = doc.querySelector(".navbar_wrapper");
+    document.querySelector(".nav_404").append(nav);
+  })
+  .then(() => {
+    tagsCount();
+    navLinksHadler();
+  })
+  .catch((error) => {
+    console.error("Error fetching page:", error);
+  });
