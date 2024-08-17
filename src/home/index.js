e3bdb2f880259d58a45ea3e336b5c4d6be13686c
Max Chechel <maksymchechel@yahoo.com>
2024-08-17 15:51:59 +0200

    Update index.js

--- src/home/index.js
+++ src/home/index.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       const [listInstance] = listInstances;
 
-      console.log(listInstances);
+      console.log(listInstances.items);
     },
   ]);
 
